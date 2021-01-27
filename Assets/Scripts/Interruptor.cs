03bf00a75ad11b3fec6f9a78f37b0494993b6c17
Fvico <ifva94@gmail.com>
2021-01-27 21:27:53 +0100

    atendiendo a UIUX

---
+++ Assets/Scripts/Interruptor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interruptor : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> _switch;
+    [SerializeField]
+    GameObject _meshOn;
+    [SerializeField]
+    GameObject _meshOff;
+    [SerializeField]
+    float _time;
+    [SerializeField]
+    bool _isTime;
+
+    private void Start()
+    {
+        _meshOff.SetActive(false);
+        _meshOn.SetActive(true);
+    }
+    
+    IEnumerator BotonForTime()
+    {
+        for (int i = 0; i < _switch.Count; i++)
+        {
+            _switch[i].SetActive(false);
+            _meshOff.SetActive(true);
+            _meshOn.SetActive(false);
+        }
+        yield return new WaitForSeconds(_time);
+        for (int i = 0; i < _switch.Count; i++)
+        {
+            _switch[i].SetActive(true);
+            _meshOff.SetActive(false);
+            _meshOn.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (_isTime)
+            {
+                StartCoroutine(BotonForTime());
+            }
+            else
+            {
+                for (int i = 0; i < _switch.Count; i++)
+                {
+                    _switch[i].SetActive(false);
+                    _meshOff.SetActive(true);
+                    _meshOn.SetActive(false);
+                }
+            }
+            
+        }
+    }
+}
