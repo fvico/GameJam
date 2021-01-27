9b75907eb035f9713d96370155e2413693383a9e
Fvico <ifva94@gmail.com>
2021-01-27 17:08:25 +0100

    Update FadeIn

---
+++ Assets/Scripts/AtraparPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtraparPlayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _player;
+    [SerializeField]
+    Transform _positionInicialPlayer;
+    [SerializeField]
+    Animator _FadeInOut;
+
+    IEnumerator ReinicioNivel()
+    {
+        _FadeInOut.SetBool("IsFadeIn", true);
+        yield return new WaitForSeconds(1.9f);
+        _player.transform.position = _positionInicialPlayer.position;
+        _FadeInOut.SetBool("IsFadeIn", false);
+
+        // yield return new WaitForSeconds(30f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ReiniciarNivel")
+        {
+            print("Cchoque");
+        }
+    }
+}
