2ba3f35531864304854d345a9cca96bfb87c5488
CharlieBaena <carlos_baena_ariza@hotmail.com>
2021-01-28 01:54:16 +0100

    creando el menu de niveles

---
+++ Assets/Scripts/Niveles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Niveles : MonoBehaviour
+{
+
+    [MenuItem("Niveles/Test")]
+    public static void SetLevelToTest()
+    {
+        SetLevel(0);
+    }
+
+    [MenuItem("Niveles/1")]
+    public static void SetLevelTo1()
+    {
+        SetLevel(1);
+    }
+
+    [MenuItem("Niveles/2")]
+    public static void SetLevelTo2()
+    {
+        SetLevel(2);
+    }
+
+    [MenuItem("Niveles/3")]
+    public static void SetLevelTo3()
+    {
+        SetLevel(3);
+    }
+
+    public static void SetLevel(int level)
+    {
+        PlayerPrefs.SetInt("ActualLevel", level);
+        GameObject padreDeTodos = GameObject.FindGameObjectWithTag("PadreDeTodos");
+        if(level < padreDeTodos.transform.childCount)
+        {
+            PlayerPrefs.SetInt("LevelToLoad", level);
+            for(int i = 0; i < padreDeTodos.transform.childCount; i++)
+            {
+                padreDeTodos.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            padreDeTodos.transform.GetChild(level).gameObject.SetActive(true);
+        }
+    }
+
+}
